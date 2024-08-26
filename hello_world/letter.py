0384fa2441862c35cf0ee01eeda9c9f22b9b05c2
PawlakMarek <26022173+PawlakMarek@users.noreply.github.com>
2024-08-26 08:24:12 +0200

    Initial commit with hello_world classes

---
+++ hello_world/letter.py
@@ -0,0 +1,7 @@
+print("13 May 2024")
+print("I'm feeling spectacular today!")
+print(
+    "I hope that learning coding will be a fun experience and that it will be beneficial in my professional career as well as useful in my hobbies."
+)
+print("I'm excited to see what the future holds for me.")
+print("🚀 🌕")
